ea52a58a7764e15691d30f5b3a44c047274c6dc6
Michael <giordm10@tcnj.edu>
2022-11-01 18:23:05 -0400

    spotipy sample code

---
+++ spotipy_tutorial.py
@@ -0,0 +1,18 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+#SPOTIPY_CLIENT_ID='914edfe2387b4fdb9de4fcd942faed5b'
+#SPOTIPY_CLIENT_SECRET='43d99df3213b4de2b190f8aa8602eb65'
+#SPOTIPY_REDIRECT_URI='https://localhost:8888/callback'
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
