3717665046230c0a5c0f49660a7af11a59890b1e
schmij12 <schmij12@tcnj.edu>
2022-11-30 14:39:30 -0500

    remove selectArtist function, move into button

--- gameNEW.py
+++ gameNEW.py
@@ -105,27 +105,7 @@ leaderboardNameEntered = False
 #ic - unhighlighted color
 #ac - highlighted color
 #action - action (function) on button click
-def button(msg,x,y,w,h,ic,ac,events, action=None):
-    clicked = False
-    for event in events:
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            clicked = True
-    global state
-    mouse = pygame.mouse.get_pos()
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
-        
-        if clicked and action != None:
-            if isinstance(action, str) == True:
-                state = action
-            else:
-                action()
-    else:
-        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
-    text = smallfont.render(msg , True , white)
-    gameDisplay.blit(text, ( (x+(w/5.5)), (y+(h/3)) ))
-
-def selectArtist(msg,x,y,w,h,ic,ac,events, artist, action=None):
+def button(msg,x,y,w,h,ic,ac,events, artist=None, action=None):
     clicked = False
     global curr_artist
     for event in events:
@@ -135,17 +115,20 @@ def selectArtist(msg,x,y,w,h,ic,ac,events, artist, action=None):
     mouse = pygame.mouse.get_pos()
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
-
+        
         if clicked and action != None:
-            curr_artist = artist
+            if artist != None:
+                curr_artist = artist
             if isinstance(action, str) == True:
                 state = action
             else:
                 action()
     else:
         pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
-
-    text = smallerfont.render(msg , True , white)
+    if artist == None:
+        text = smallfont.render(msg , True , white)
+    else:
+        text = smallerfont.render(msg , True , white)
     gameDisplay.blit(text, ( (x+(w/5.5)), (y+(h/3)) ))
 
 def start():
@@ -303,11 +286,11 @@ def loop():
 def mainMenu(events):
     settingMenu = False
     title()
-    button("Start 2 Player", 670, 360, 230, 50, color_dark, color_light, events, "twoPlayer")
-    button("Start 1 player", 400, 360, 230, 50, color_dark, color_light, events, "onePlayer")
-    button("Leaderboard", 400, 470, 230, 50, color_dark, color_light, events, "leaderboard")
-    button("Settings", 670, 470, 230, 50, color_dark, color_light, events, "settingsMenu")
-    button("Quit", 0, 470, 130, 50, color_dark, color_light, events, end)
+    button("Start 2 Player", 670, 360, 230, 50, color_dark, color_light, events, action="twoPlayer")
+    button("Start 1 player", 400, 360, 230, 50, color_dark, color_light, events, action="onePlayer")
+    button("Leaderboard", 400, 470, 230, 50, color_dark, color_light, events, action="leaderboard")
+    button("Settings", 670, 470, 230, 50, color_dark, color_light, events, action="settingsMenu")
+    button("Quit", 0, 470, 130, 50, color_dark, color_light, events, action=end)
               
     #make switch case that checks current state, then calls each state's respective function. should be alot cleaner code
     
@@ -321,8 +304,8 @@ def end():
 
 def randomSong(events, text):
     gameDisplay.blit(text, ((0+(50/2)), (100+(50/2))))
-    button("Next song", 40, 470, 200, 50, color_dark, color_light, events, "nextSong")
-    button("Quit", 670, 470, 130, 50, color_dark, color_light, events, end)
+    button("Next song", 40, 470, 200, 50, color_dark, color_light, events, action="nextSong")
+    button("Quit", 670, 470, 130, 50, color_dark, color_light, events, action=end)
     textinput.update(events)
     # Blit its surface onto the screen
     gameDisplay.blit(textinput.surface, (300, 300))
@@ -331,8 +314,8 @@ def randomSong(events, text):
 def randomSong2(events, text, turnText):
     gameDisplay.blit(text, ((0+(50/2)), (100+(50/2))))
     gameDisplay.blit(turnText, ((0+(50/2)), (200+(50/2))) )
-    button("Next song", 40, 470, 200, 50, color_dark, color_light, events, "nextSong2")
-    button("Quit", 670, 470, 130, 50, color_dark, color_light, events, end)
+    button("Next song", 40, 470, 200, 50, color_dark, color_light, events, action="nextSong2")
+    button("Quit", 670, 470, 130, 50, color_dark, color_light, events, action=end)
     textinput.update(events)
     # Blit its surface onto the screen
     gameDisplay.blit(textinput.surface, (300, 300))
@@ -343,30 +326,30 @@ def onePlayer(events):
     gameDisplay.blit(textOnePlay, ((0+(50/2)), (100+(50/2))))
     #textinput.update(events)
     #gameDisplay.blit(textinput.surface, (300, 300))
-    selectArtist("Nothing But Thieves", 150, 220, 230, 50, color_dark, color_light, events, "Nothing But Thieves", "randomSong")
-    selectArtist("Men I trust", 150, 280, 230, 50, color_dark, color_light, events, "Men I trust", "randomSong")
-    selectArtist("Arctic Monkeys", 150, 340, 230, 50, color_dark, color_light, events, "Arctic Monkeys", "randomSong")
-    selectArtist("Phish", 150, 400, 230, 50, color_dark, color_light, events, "Phish", "randomSong")
-    selectArtist("Billy Joel", 150, 460, 230, 50, color_dark, color_light, events, "Billy Joel", "randomSong")
-    selectArtist("Cigarettes After Sex", 400, 220, 230, 50, color_dark, color_light, events, "Cigarettes After Sex", "randomSong")
-    selectArtist("The Neighbourhood", 400, 280, 230, 50, color_dark, color_light, events, "The Neighbourhood", "randomSong")
-    selectArtist("Five Finger Death Punch", 400, 340, 230, 50, color_dark, color_light, events, "Five Finger Death Punch", "randomSong")
-    selectArtist("Coldplay", 400, 400, 230, 50, color_dark, color_light, events, "Coldplay", "randomSong")
-    selectArtist("Red Hot Chilli Peppers", 400, 460, 230, 50, color_dark, color_light, events, "Red Hot Chilli Peppers", "randomSong")
-    selectArtist("Jimi Hendrix", 650, 220, 230, 50, color_dark, color_light, events, "Jimi Hendrix", "randomSong")
-    selectArtist("Glue Trip", 650, 280, 230, 50, color_dark, color_light, events, "Glue Trip", "randomSong")
-    selectArtist("City and Colour", 650, 340, 230, 50, color_dark, color_light, events, "City and Colour", "randomSong")
-    selectArtist("Halestorm", 650, 400, 230, 50, color_dark, color_light, events, "Halestorm", "randomSong")
-    selectArtist("Diiv", 650, 460, 230, 50, color_dark, color_light, events, "Diiv", "randomSong")
-    selectArtist("Green Day", 900, 220, 230, 50, color_dark, color_light, events, "Green Day", "randomSong")
-    selectArtist("Crumb", 900, 280, 230, 50, color_dark, color_light, events, "Crumb", "randomSong")
-    selectArtist("Peach Pit", 900, 340, 230, 50, color_dark, color_light, events, "Peach Pit", "randomSong")
-    selectArtist("The Fray", 900, 400, 230, 50, color_dark, color_light, events, "The Fray", "randomSong")
-    selectArtist("Lil Nas X", 900, 460, 230, 50, color_dark, color_light, events, "Lil Nas X", "randomSong")
+    button("Nothing But Thieves", 150, 220, 230, 50, color_dark, color_light, events, "Nothing But Thieves", "randomSong")
+    button("Men I trust", 150, 280, 230, 50, color_dark, color_light, events, "Men I trust", "randomSong")
+    button("Arctic Monkeys", 150, 340, 230, 50, color_dark, color_light, events, "Arctic Monkeys", "randomSong")
+    button("Phish", 150, 400, 230, 50, color_dark, color_light, events, "Phish", "randomSong")
+    button("Billy Joel", 150, 460, 230, 50, color_dark, color_light, events, "Billy Joel", "randomSong")
+    button("Cigarettes After Sex", 400, 220, 230, 50, color_dark, color_light, events, "Cigarettes After Sex", "randomSong")
+    button("The Neighbourhood", 400, 280, 230, 50, color_dark, color_light, events, "The Neighbourhood", "randomSong")
+    button("Five Finger Death Punch", 400, 340, 230, 50, color_dark, color_light, events, "Five Finger Death Punch", "randomSong")
+    button("Coldplay", 400, 400, 230, 50, color_dark, color_light, events, "Coldplay", "randomSong")
+    button("Red Hot Chilli Peppers", 400, 460, 230, 50, color_dark, color_light, events, "Red Hot Chilli Peppers", "randomSong")
+    button("Jimi Hendrix", 650, 220, 230, 50, color_dark, color_light, events, "Jimi Hendrix", "randomSong")
+    button("Glue Trip", 650, 280, 230, 50, color_dark, color_light, events, "Glue Trip", "randomSong")
+    button("City and Colour", 650, 340, 230, 50, color_dark, color_light, events, "City and Colour", "randomSong")
+    button("Halestorm", 650, 400, 230, 50, color_dark, color_light, events, "Halestorm", "randomSong")
+    button("Diiv", 650, 460, 230, 50, color_dark, color_light, events, "Diiv", "randomSong")
+    button("Green Day", 900, 220, 230, 50, color_dark, color_light, events, "Green Day", "randomSong")
+    button("Crumb", 900, 280, 230, 50, color_dark, color_light, events, "Crumb", "randomSong")
+    button("Peach Pit", 900, 340, 230, 50, color_dark, color_light, events, "Peach Pit", "randomSong")
+    button("The Fray", 900, 400, 230, 50, color_dark, color_light, events, "The Fray", "randomSong")
+    button("Lil Nas X", 900, 460, 230, 50, color_dark, color_light, events, "Lil Nas X", "randomSong")
     # curr_artist = textinput.value
-    button("Start", 450, 570, 130, 50, color_dark, color_light, events, "randomSong")
-    button("Back", 585, 570, 130, 50, color_dark, color_light, events, "mainMenu")
-    button("Quit", 720, 570, 130, 50, color_dark, color_light, events, end)
+    button("Start", 450, 570, 130, 50, color_dark, color_light, events, action="randomSong")
+    button("Back", 585, 570, 130, 50, color_dark, color_light, events, action="mainMenu")
+    button("Quit", 720, 570, 130, 50, color_dark, color_light, events, action=end)
 
 def twoPlayer(events):
    global curr_artist
@@ -377,29 +360,29 @@ def twoPlayer(events):
 #    textinput.update(events)
    gameDisplay.blit(textinput.surface, (300, 300))
 #    curr_artist = textinput.value
-   selectArtist("Nothing But Thieves", 150, 220, 230, 50, color_dark, color_light, events, "Nothing But Thieves", "randomSong2")
-   selectArtist("Men I trust", 150, 280, 230, 50, color_dark, color_light, events, "Men I trust", "randomSong2")
-   selectArtist("Arctic Monkeys", 150, 340, 230, 50, color_dark, color_light, events, "Arctic Monkeys", "randomSong2")
-   selectArtist("Phish", 150, 400, 230, 50, color_dark, color_light, events, "Phish", "randomSong2")
-   selectArtist("Billy Joel", 150, 460, 230, 50, color_dark, color_light, events, "Billy Joel", "randomSong2")
-   selectArtist("Cigarettes After Sex", 400, 220, 230, 50, color_dark, color_light, events, "Cigarettes After Sex", "randomSong2")
-   selectArtist("The Neighbourhood", 400, 280, 230, 50, color_dark, color_light, events, "The Neighbourhood", "randomSong2")
-   selectArtist("Five Finger Death Punch", 400, 340, 230, 50, color_dark, color_light, events, "Five Finger Death Punch", "randomSong2")
-   selectArtist("Coldplay", 400, 400, 230, 50, color_dark, color_light, events, "Coldplay", "randomSong2")
-   selectArtist("Red Hot Chilli Peppers", 400, 460, 230, 50, color_dark, color_light, events, "Red Hot Chilli Peppers", "randomSong2")
-   selectArtist("Jimi Hendrix", 650, 220, 230, 50, color_dark, color_light, events, "Jimi Hendrix", "randomSong2")
-   selectArtist("Glue Trip", 650, 280, 230, 50, color_dark, color_light, events, "Glue Trip", "randomSong2")
-   selectArtist("City and Colour", 650, 340, 230, 50, color_dark, color_light, events, "City and Colour", "randomSong2")
-   selectArtist("Halestorm", 650, 400, 230, 50, color_dark, color_light, events, "Halestorm", "randomSong2")
-   selectArtist("Diiv", 650, 460, 230, 50, color_dark, color_light, events, "Diiv", "randomSong2")
-   selectArtist("Green Day", 900, 220, 230, 50, color_dark, color_light, events, "Green Day", "randomSong2")
-   selectArtist("Crumb", 900, 280, 230, 50, color_dark, color_light, events, "Crumb", "randomSong2")
-   selectArtist("Peach Pit", 900, 340, 230, 50, color_dark, color_light, events, "Peach Pit", "randomSong2")
-   selectArtist("The Fray", 900, 400, 230, 50, color_dark, color_light, events, "The Fray", "randomSong2")
-   selectArtist("Lil Nas X", 900, 460, 230, 50, color_dark, color_light, events, "Lil Nas X", "randomSong2")
-   button("Start", 450, 570, 130, 50, color_dark, color_light, events, "randomSong")
-   button("Back", 585, 570, 130, 50, color_dark, color_light, events, "mainMenu")
-   button("Quit", 720, 570, 130, 50, color_dark, color_light, events, end)
+   button("Nothing But Thieves", 150, 220, 230, 50, color_dark, color_light, events, "Nothing But Thieves", "randomSong2")
+   button("Men I trust", 150, 280, 230, 50, color_dark, color_light, events, "Men I trust", "randomSong2")
+   button("Arctic Monkeys", 150, 340, 230, 50, color_dark, color_light, events, "Arctic Monkeys", "randomSong2")
+   button("Phish", 150, 400, 230, 50, color_dark, color_light, events, "Phish", "randomSong2")
+   button("Billy Joel", 150, 460, 230, 50, color_dark, color_light, events, "Billy Joel", "randomSong2")
+   button("Cigarettes After Sex", 400, 220, 230, 50, color_dark, color_light, events, "Cigarettes After Sex", "randomSong2")
+   button("The Neighbourhood", 400, 280, 230, 50, color_dark, color_light, events, "The Neighbourhood", "randomSong2")
+   button("Five Finger Death Punch", 400, 340, 230, 50, color_dark, color_light, events, "Five Finger Death Punch", "randomSong2")
+   button("Coldplay", 400, 400, 230, 50, color_dark, color_light, events, "Coldplay", "randomSong2")
+   button("Red Hot Chilli Peppers", 400, 460, 230, 50, color_dark, color_light, events, "Red Hot Chilli Peppers", "randomSong2")
+   button("Jimi Hendrix", 650, 220, 230, 50, color_dark, color_light, events, "Jimi Hendrix", "randomSong2")
+   button("Glue Trip", 650, 280, 230, 50, color_dark, color_light, events, "Glue Trip", "randomSong2")
+   button("City and Colour", 650, 340, 230, 50, color_dark, color_light, events, "City and Colour", "randomSong2")
+   button("Halestorm", 650, 400, 230, 50, color_dark, color_light, events, "Halestorm", "randomSong2")
+   button("Diiv", 650, 460, 230, 50, color_dark, color_light, events, "Diiv", "randomSong2")
+   button("Green Day", 900, 220, 230, 50, color_dark, color_light, events, "Green Day", "randomSong2")
+   button("Crumb", 900, 280, 230, 50, color_dark, color_light, events, "Crumb", "randomSong2")
+   button("Peach Pit", 900, 340, 230, 50, color_dark, color_light, events, "Peach Pit", "randomSong2")
+   button("The Fray", 900, 400, 230, 50, color_dark, color_light, events, "The Fray", "randomSong2")
+   button("Lil Nas X", 900, 460, 230, 50, color_dark, color_light, events, "Lil Nas X", "randomSong2")
+   button("Start", 450, 570, 130, 50, color_dark, color_light, events, action="randomSong")
+   button("Back", 585, 570, 130, 50, color_dark, color_light, events, action="mainMenu")
+   button("Quit", 720, 570, 130, 50, color_dark, color_light, events, action=end)
 
 def title():
     titleText = largefont.render("Guess That Song!" , True , white)
@@ -408,8 +391,8 @@ def title():
 def setting(events):
     settingText = smallfont.render("Setting Menu", True, white)
     gameDisplay.blit(settingText, ((970+(50/2)), (100+(50/2))))
-    button("Main Menu", 670, 470, 200, 50, color_dark, color_light, events, "mainMenu")
-    button("Quit", 0, 470, 130, 50, color_dark, color_light, events, end)
+    button("Main Menu", 670, 470, 200, 50, color_dark, color_light, events, action="mainMenu")
+    button("Quit", 0, 470, 130, 50, color_dark, color_light, events, action=end)
 
 def gameOver(events):
     global leaderboardInformation
@@ -430,8 +413,8 @@ def gameOver(events):
         nameEntered = smallfont.render("Your name is now in the leaderboard", True, white)
         gameDisplay.blit(nameEntered, (300, 350))
 
-    button("Main Menu", 270, 470, 200, 50, color_dark, color_light, events, "mainMenu")
-    button("Quit", 670, 470, 130, 50, color_dark, color_light, events, end)
+    button("Main Menu", 270, 470, 200, 50, color_dark, color_light, events, action="mainMenu")
+    button("Quit", 670, 470, 130, 50, color_dark, color_light, events, action=end)
 
 def leaderboard(events):
     global leaderboardInformation
@@ -454,8 +437,8 @@ def leaderboard(events):
         else:
             yAxis += 40
         placement += 1
-    button("Main Menu", 270, 590, 200, 50, color_dark, color_light, events, "mainMenu")
-    button("Quit", 670, 590, 130, 50, color_dark, color_light, events, end)
+    button("Main Menu", 270, 590, 200, 50, color_dark, color_light, events, action="mainMenu")
+    button("Quit", 670, 590, 130, 50, color_dark, color_light, events, action=end)
 
 def updateLeaderboard():
     global infoDict
