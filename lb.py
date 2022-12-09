bb2b538afb5984ec7fa2561717e67261c9ade788
schmij12 <schmij12@tcnj.edu>
2022-12-08 19:44:46 -0500

    fix leaderboard bug

--- lb.py
+++ lb.py
@@ -2,10 +2,11 @@ import csv
 
 def read_text():
     with open('leaderboard.csv', newline='') as csvfile:
-        infoReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        infoReader = csv.reader(csvfile, delimiter='\n', quotechar='|')
         infoDict = dict()
         for row in infoReader:
             row = str(row[0]).split(",")
+            print(row)
             # print(row[0])
             # print(row[1])
             infoDict[row[0]] = int(row[1])
